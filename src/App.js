43727c58b9f5f501244b55327f89f240024de2ea
Tedla B <mekdi.td87@outlook.com>
2020-03-26 00:30:27 -0500

    changes in component file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Dream Vacation Destinations!</h1>
+      <ul>
+        <li>first li</li>
+        <li>second li</li>
+        <li>third li</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/secondApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+
+function SecondApp() {
+  return (
+    <div>
+      <h1>My Dream Vacation Destinations!</h1>
+      <p>This is paragraph!</p>
+    </div>
+  );
+}
+
+export default SecondApp;
