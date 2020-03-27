0fce0f260dac4d6b830f1e6e0539cb579d199777
Tedla B <mekdi.td87@outlook.com>
2020-03-27 00:20:23 -0500

    modified JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,20 @@ import React from 'react';
 
 
 function Header() {
+    const style = {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'end',
+        paddingLeft: '5px'
+         }
     return (
         <div>
             <img src='./Asset/Logo.PNG' border='0.5' alt='help' width='30' height='30' />
-            <ul className = 'Navbar' style={{FlexDirection: 'column', textAlign: 'right' }}>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Login</li>
+            <ul className = 'Navbar' >
+                <li style = { style }>About</li>
+                <li style = { style }>Contact</li>
+                <li style = { style }>Login</li>
             </ul>
         </div>
     )
